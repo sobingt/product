b24d705c3d7379613eff2b55e38faf007bf01bed
sagar2526 <sagarhinge01@gmail.com>
2018-08-27 14:27:35 +0530

    basic files and product json added

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json())
+var products[
+	{
+		id: 1,
+		name: 'headphones',
+		category: 'accessories',
+	},
+	{
+		id: 2,
+		name: 'bluetooth-headphones',
+		category: 'accessories',
+	},
+	{
+		id: 3,
+		name: 'Mobiles',
+		category: 'Electronics',
+	},
+    {
+		id: 4,
+		name: 'Televisions',
+		category: 'Electronics',
+	},
+	{
+		id: 5,
+		name: 'Shirts',
+		category: 'Clothing',
+	},
+	{
+		id: 6,
+		name: 'Laptop',
+		category: 'Electronics',
+	}
+]
