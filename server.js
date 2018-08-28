e8a3f408606e3b37d759f9a4987e835b7af8dd0a
sagar2526 <sagarhinge01@gmail.com>
2018-08-28 14:31:42 +0530

    new GET api call is created with mongo db

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json())
-var products=[
+
+mongoose.connect('mongodb://localhost:27017/shop')
+mongoose.connection.on('error', (error) => console.error(error))
+mongoose.connection.on('open', () => console.log("success in connecting to mongodb"))
+ 
+var productsSchema = new mongoose.Schema({
+	name: String,
+	category: String
+})
+
+var Products = mongoose.model('Products', productsSchema)
+
+var productsArray=[
 	{
 		id: 1,
 		name: 'headphones',
@@ -56,7 +70,7 @@ app.get('/api/products/:id',(request,response)=>{
   response.json(prod)
 })
 
-app.post('/api/products',(request,response)=>{
+/*app.post('/api/products',(request,response)=>{
 	console.log(request.body)
 	products.push({
 
@@ -89,5 +103,17 @@ app.del('/api/products/:id',(request,response)=>{
 		}
 	});
 	response.json(products)
+})*/
+
+app.post('/api/product',(request,response)=>{ 
+	console.log(request.body)
+    const product = new Products({
+        name: request.body.name,
+        category: request.body.category
+    })
+    product.save().then((product) => {
+        console.log('Added successfully')
+        response.json(product)
+    })
 })
 app.listen(4040,()=> console.log('Express server at 4040'))
